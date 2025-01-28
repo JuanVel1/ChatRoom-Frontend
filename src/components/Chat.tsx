2aeb0c5c82f1fb3c7c5053ebdcf1fe32e8b5cd01
Juan Jose Velasquez Largo <100188608+JuanVel1@users.noreply.github.com>
2025-01-28 10:27:32 -0500

    Updating final img path

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -52,7 +52,7 @@ const Chat = () => {
         <div className="flex items-center">
           <h1 className="ballet-title text-6xl p-5">Chat room</h1>
           <img
-            src="src/assets/CS_Star_2.svg"
+            src="https://github.com/JuanVel1/ChatRoom-Frontend/blob/main/src/assets/CS_Star_2.svg"
             width={40}
             className="animated"
             alt="a star moving"
